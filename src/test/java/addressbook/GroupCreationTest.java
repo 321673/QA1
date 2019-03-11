63fc3d29797d46e1da13ca2739ae48da4f05709c
321673 <>
2019-03-11 23:04:40 +0300

    Extracted GroupData Class

--- src/test/java/addressbook/GroupCreationTest.java
+++ src/test/java/addressbook/GroupCreationTest.java
@@ -1,16 +1,16 @@
 package addressbook;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
 
 public class GroupCreationTest {
     private WebDriver driver;
-    private String baseUrl;
     private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
     private SelenoidWebDriverProvider swdp;
@@ -20,32 +20,60 @@ public class GroupCreationTest {
     public void setUp() throws Exception {
         swdp = new SelenoidWebDriverProvider();
         driver = swdp.createDriver();
-        baseUrl = "https://www.katalon.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("http://localhost/addressbook/");
+        login("admin", "secret");
+    }
+
+    public void login(String username, String password) {
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).clear();
+        driver.findElement(By.name("user")).sendKeys(username);
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).clear();
+        driver.findElement(By.name("pass")).sendKeys(password);
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
     }
 
     @Test
     public void testGroups() throws Exception {
-        driver.get("http://localhost/addressbook/");
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys("secret");
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-        driver.findElement(By.linkText("groups")).click();
-        driver.findElement(By.name("new")).click();
+
+        gotoGroupPage("groups");
+        initGroupCreation("new");
+        fillGroupForm(new GreoupData("Name", "Header", "Footer"));
+        submitGroupCreaton();
+        returnToGroupPage("group page");
+        logout("Logout");
+    }
+
+    public void logout(String logout) {
+        driver.findElement(By.linkText(logout)).click();
+    }
+
+    public void returnToGroupPage(String s) {
+        driver.findElement(By.linkText(s)).click();
+    }
+
+    public void submitGroupCreaton() {
+        driver.findElement(By.name("submit")).click();
+    }
+
+    public void fillGroupForm(GreoupData greoupData) {
         driver.findElement(By.name("group_name")).click();
         driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys("Name");
+        driver.findElement(By.name("group_name")).sendKeys(greoupData.getName());
         driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys("Header");
+        driver.findElement(By.name("group_header")).sendKeys(greoupData.getHeader());
         driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys("Footer");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
-        driver.findElement(By.linkText("Logout")).click();
+        driver.findElement(By.name("group_footer")).sendKeys(greoupData.getFooter());
+    }
+
+    public void initGroupCreation(String s) {
+        driver.findElement(By.name(s)).click();
+    }
+
+    public void gotoGroupPage(String groups) {
+        driver.findElement(By.linkText(groups)).click();
     }
 
     @AfterClass(alwaysRun = true)
